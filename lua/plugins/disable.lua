e43506a12753c30643e5d509a618636e79fc7c3f
Summer-Field <xiaye.field@gmail.com>
2023-11-02 16:44:39 +0800

    Now I Use LAZYVIM💤
    1. Save the old state
    2. Fully migrato to LAZYVIM

--- lua/plugins/disable.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  { "echasnovski/mini.comment", enabled = false },
-}
--- lua/plugins/tabular.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("v", "ga", ":Tabular /", { desc = "Tab Alignment" })
-return {
-  "godlygeek/tabular",
-  keys = { "ga" },
-}
