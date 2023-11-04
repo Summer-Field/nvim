8098f704d465ccceba760369d670ef9a1f4dc868
Summer-Field <xiaye.field@gmail.com>
2023-11-04 17:44:02 +0800

    Config Joshuto In lua way
    * joshuto.lua
    1. Config Joshuto in lua instead of vimscript

--- lua/plugins/joshuto.lua
+++ lua/plugins/joshuto.lua
@@ -2,10 +2,10 @@ return {
   "theniceboy/joshuto.nvim",
   lazy = true,
   cmd = "Joshuto",
+  keys = {
+    { "R", "<cmd>Joshuto<cr>", desc = "File Navigator" },
+  },
   config = function()
-    -- let g:joshuto_floating_window_winblend = 0
-    -- let g:joshuto_floating_window_scaling_factor = 1.0
-    -- let g:joshuto_use_neovim_remote = 1 " for neovim-remote support
     vim.g.joshuto_floating_window_scaling_factor = 1.0
     vim.g.joshuto_use_neovim_remote = 1
     vim.g.joshuto_floating_window_winblend = 0
