f471a11198a165a519aba722f133793d5edfbfb2
Summer-Field <xiaye.field@gmail.com>
2023-11-04 17:45:33 +0800

    Rename surround.lua To nvim-surround.lua
    *nvim-surround.lua
    1. Rename to nvim-surround.lua
    2. Fix a warning in config by removing comments in function

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -3,8 +3,7 @@ return {
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
   config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
+    -- Configuration here, or leave {} empty to use defaults
+    require("nvim-surround").setup()
   end,
 }
