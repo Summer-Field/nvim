3b7c073682ae03ba8b7ec64b73433fd2d6024eb0
Summer-Field <xiaye.field@gmail.com>
2023-11-04 17:37:01 +0800

    Update Comment Plugin
    * comment.lua:
    1. Use comment.nvim instead of tcomment.vim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,11 +1,6 @@
-vim.g.tcomment_textobject_inlinecomment = ""
-vim.keymap.set("n", "ci", "cl", { remap = false })
-
-vim.keymap.set("n", "<leader>cn", "<Plug>TComment_Commentc", { desc = "Comment" })
-vim.keymap.set("v", "<leader>cn", "<Plug>TComment_Commentb", { desc = "Comment" })
-vim.keymap.set("n", "<leader>cu", "<Plug>TComment_Uncommentc", { desc = "Uncomment" })
-vim.keymap.set("v", "<leader>cu", "<Plug>TComment_Uncommentb", { desc = "Uncomment" })
-
 return {
-  "tomtom/tcomment_vim",
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+  },
 }
