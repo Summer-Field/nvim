1f1bcbab126ef4c9ddad82c0f3917872450c1f0a
Summer-Field <xiaye.field@gmail.com>
2023-11-04 17:41:24 +0800

    Update Disable Plugins
    * disable.lua
    1. Disable mini.bufremove
    2. Enable neo-tree.nvim

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "mini.bufremove", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
